3ffd04da176fdcb919a9dff288a3be528b752433
Alex <masiuc1.19@gmail.com>
2020-06-06 10:52:48 +0300

    Практика 8

--- riddles/views.py
+++ riddles/views.py
@@ -8,6 +8,8 @@ from datetime import datetime
 # сообщения
 from .models import Mark
 
+import numpy
+
 
 # главная страница со списком загадок
 def index(request):
@@ -32,22 +34,53 @@ def detail(request, riddle_id):
     error_message = None
     if "error_message" in request.GET:
         error_message = request.GET["error_message"]
+    # формируем список ответов
+    ordered_option_set = \
+        list(Option.objects.filter(riddle_id=riddle_id))
+    # формируем случайный порядок номеров ответов
+    option_iter = \
+        numpy.random.permutation(len(ordered_option_set))
+    # формируем новый список, в который выписываем ответы
+    # в сформированном случайном порядке
+    option_set = []
+    for num in option_iter:
+        option_set.append(ordered_option_set[num])
     return render(
         request,
         "answer.html",
         {
-            "riddle": get_object_or_404(Riddle, pk=riddle_id),
-            "error_message": error_message,
-            "latest_messages": Message.objects.filter(chat_id=riddle_id).order_by('-pub_date')[:5],
-            # кол-во оценок, выставленных пользователем
-            "already_rated_by_user": Mark.objects .filter(author_id=request.user.id) .count(),
-            # оценка текущего пользователя
-            "user_rating": Mark.objects .filter(author_id=request.user.id) .filter(riddle_id=riddle_id) .aggregate(Avg('mark'))["mark__avg"],
-            # средняя по всем пользователям оценка
-            "avg_mark": Mark.objects .filter(riddle_id=riddle_id) .aggregate(Avg('mark'))["mark__avg"]
+            # передаем список ответов в случайном порядке
+            "option_set": option_set,
 
+            "riddle": get_object_or_404(
+                Riddle, pk=riddle_id),
+            "error_message": error_message,
+            "latest_messages":
+                Message.objects
+                    .filter(chat_id=riddle_id)
+                    .order_by('-pub_date')[:5],
+            # кол-во оценок, выставленных пользователем
+            "already_rated_by_user":
+                Mark.objects
+                    .filter(author_id=request.user.id)
+                    .filter(riddle_id=riddle_id)
+                    .count(),
+            # оценка текущего пользователя
+            "user_rating":
+                Mark.objects
+                    .filter(author_id=request.user.id)
+                    .filter(riddle_id=riddle_id)
+                    .aggregate(Avg('mark'))
+                    ["mark__avg"],
+            # средняя по всем пользователям оценка
+            "avg_mark":
+                Mark.objects
+                    .filter(riddle_id=riddle_id)
+                    .aggregate(Avg('mark'))
+                    ["mark__avg"]
         }
     )
+
 
 
 # для ответа на асинхронный запрос в формате JSON
@@ -236,8 +269,56 @@ def post_mark(request, riddle_id):
 
 
 def get_mark(request, riddle_id):
-    res = Mark.objects\
-            .filter(riddle_id=riddle_id)\
-            .aggregate(Avg('mark'))
+    res = Mark.objects \
+        .filter(riddle_id=riddle_id) \
+        .aggregate(Avg('mark'))
 
     return JsonResponse(json.dumps(res), safe=False)
+
+
+def admin(request):
+    message = None
+    if "message" in request.GET:
+        message = request.GET["message"]
+    # создание HTML-страницы по шаблону admin.html
+    # с заданными параметрами latest_riddles и message
+    return render(
+        request,
+        "admin.html",
+        {
+            "latest_riddles":
+                Riddle.objects.order_by('-pub_date')[:5],
+            "message": message,
+        }
+    )
+
+
+def post_riddle(request):
+    # защита от добавления загадок неадминистраторами
+    author = request.user
+    if not (author.is_authenticated and author.is_staff):
+        return HttpResponseRedirect(app_url + "admin")
+    # добавление загадки
+    rid = Riddle()
+    rid.riddle_text = request.POST['text']
+    rid.pub_date = datetime.now()
+    rid.save()
+    # добавление вариантов ответа
+    i = 1  # нумерация вариантов на форме начинается с 1
+    # количество вариантов неизвестно,
+    # поэтому ожидаем возникновение исключения,
+    # когда варианты кончатся
+    try:
+        while request.POST['option' + str(i)]:
+            opt = Option()
+            opt.riddle = rid
+            opt.text = request.POST['option' + str(i)]
+            opt.correct = (i == 1)
+            opt.save()
+            i += 1
+    # это ожидаемое исключение,
+    # при котором ничего делать не надо
+    except:
+        pass
+
+    return HttpResponseRedirect(app_url + str(rid.id))
